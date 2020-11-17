2277d7025de043102a095e0bb920fa59f4006191
bangnokia <bangnokia@gmail.com>
2020-11-18 02:34:40 +0700

    first post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,8 +1,10 @@
 <x-layout :title="$post->title">
 
-    <h1 class="text-teal-500 text-xl">{{ $post->title }}</h1>
+	<div id="post">
+	    <h1 class="text-red-500 text-3xl">{{ $post->title }}</h1>
+	    <div class="mt-5 leading-7 post-content">
+	        {!! $post->content !!}
+	    </div>
+	</div>
 
-    <div class="mt-5 leading-7 post-content">
-        {!! $post->content !!}
-    </div>
 </x-layout>
