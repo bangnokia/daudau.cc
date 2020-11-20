f71eae85b33a3baf09b71c3793a5b004a8912b1d
bangnokia <bangnokia@gmail.com>
2020-11-20 09:35:42 +0700

    update home page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,7 +2,7 @@
 	<ul>
 		@foreach($posts as $post)
 			<li>
-				<a href="{{ $post->link() }}">{{ $post->title }}</a>
+				- <a href="{{ $post->link() }}">{{ $post->title }}</a>
 				<span class="text-gray-500">[{{ $post->created_at }}]</span>
 			</li>
 		@endforeach
