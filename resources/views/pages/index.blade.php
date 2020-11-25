6cb6a8f7c2b47bdcf7c743bef577699129fc144c
bangnokia <bangnokia@gmail.com>
2020-11-25 10:08:20 +0700

    build asset

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,8 +2,8 @@
 	<ul>
 		@foreach($posts as $post)
 			<li>
-				- <a href="{{ $post->link() }}">{{ $post->title }}</a>
-				<span class="text-gray-500">[{{ $post->created_at }}]</span>
+				<a href="{{ $post->link() }}">{{ $post->title }}</a>
+				- <time class="text-gray-500">{{ $post->created_at }}</time>
 			</li>
 		@endforeach
 	</ul>
