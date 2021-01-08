9ab3611032531d1e19260b25dd61725ef179338d
bangnokia <bangnokia@gmail.com>
2021-01-08 10:54:56 +0700

    build asset

--- resources/views/pages/wakatime.blade.php
+++ resources/views/pages/wakatime.blade.php
@@ -1,5 +1,5 @@
 <x-layout title="wakatime" full-width="true">
-	<div class="grid grid-cols-1 gap-5">
+	<div class="grid grid-cols-2 gap-5">
 		<embed src="https://wakatime.com/share/@bangnokia/bf951775-fe7d-4c55-a47b-fcf001a44d78.svg"></embed>
 
 		<embed src="https://wakatime.com/share/@bangnokia/4311f671-d8db-42ff-8252-a12749f595d5.svg"></embed>
