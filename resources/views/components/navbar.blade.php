f0df3c7f6a1b98ac35be2c748e83add10bd0e386
bangnokia <bangnokia@gmail.com>
2021-01-16 23:39:06 +0700

    work in progress

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
 <div class="flex space-x-5">
-	<a href="/" class="text-cyan-500 hover:underline">Home</a>
-	<a href="/wakatime.html" class="text-cyan-500 hover:underline">wakatime</a>
+	<a href="/" class="text-gray-500 hover:underline">Home</a>
+	<a href="/wakatime.html" class="text-gray-500 hover:underline">wakatime</a>
 </div>
