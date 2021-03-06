3d764b177f23f944504fe10ea7b11eee41b77f4e
bangnokia <bangnokia@gmail.com>
2021-03-06 21:13:35 +0700

    build open sources page

---
+++ resources/views/pages/open-sources.blade.php
@@ -0,0 +1,11 @@
+<x-layout title="Open sources">
+
+    <h1 class="text-3xl text-cyan-500">My stuffs</h1>
+    <p>Some cool stuffs writing on my freetimes.</p>
+
+    <div class="grid mt-10 space-y-5 grid-cols-1">
+        @foreach(['laravel-serve-livereload', 'pinker', 'pekyll'] as $name)
+            <x-github-repo :name="$name" />
+        @endforeach
+    </div>
+</x-layout>
