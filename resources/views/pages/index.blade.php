5500bd0cf461935de1a92887af54c011c8b07753
bangnokia <bangnokia@gmail.com>
2021-03-20 10:38:51 +0700

    add highlight js

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,8 +3,10 @@
         @foreach($posts as $post)
             @if ($post->status === 'published')
                 <li class="mt-5">
+                    <div class="block text-gray-500 text-base">
+                        [<time datetime="{{ $post->created_at }}">{{ $post->created_at }}</time>]
+                    </div>
                     <a href="{{ $post->link() }}" class="text-lg">{{ $post->title }}</a>
-                    [<time datetime="{{ $post->created_at }}" class="text-gray-500">{{ $post->created_at }}</time>]
                 </li>
             @endif
 		@endforeach
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,4 +1,5 @@
 <x-layout :title="$post->title" :description="$post->description()">
+    <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.6.0/styles/default.min.css">
 
 	<div id="post">
 	    <h1 class="text-cyan-600 text-3xl mb-10 text-bold">{{ $post->title }}</h1>
@@ -6,5 +7,9 @@
 	        {!! $post->content !!}
 	    </div>
 	</div>
+
+    <script src="https://unpkg.com/@highlightjs/cdn-assets@10.6.0/highlight.min.js" async></script>
+    <script src="https://unpkg.com/@highlightjs/cdn-assets@10.6.0/languages/php.min.js"></script>
+    <script>hljs.highlightAll();</script>
 
 </x-layout>
