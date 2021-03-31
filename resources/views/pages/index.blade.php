b489e3b81a488d49af95f0ad35d6e1e585a7a08b
bangnokia <bangnokia@gmail.com>
2021-03-31 22:36:44 +0700

    work in progress

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,7 +5,7 @@
                 <li class="mt-5">
                     <div class="block text-sm flex space-x-5 content-end">
                         <div class="text-gray-400">[<time datetime="{{ $post->created_at }}">{{ $post->created_at }}</time>]</div>
-                        <div class="text-gray-600">{{ implode(',', $post->tags ?? []) }}</div>
+                        <div class="text-gray-600">{{ implode(', ', $post->tags ?? []) }}</div>
                     </div>
 
                     <a href="{{ $post->link() }}" class="text-lg">{{ $post->title }}</a>
