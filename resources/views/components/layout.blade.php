8364fdc6939e59950a108c3e9390506328c5c72a
bangnokia <bangnokia@gmail.com>
2021-04-07 11:50:24 +0700

    work in progress

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -26,7 +26,7 @@
 
 		<footer class="text-sm flex justify-end h-12 border-t pt-3 mt-10 space-x-2">
 			<a href="/about.html" title="github" class="text-gray-500">about</a>
-			<span>|</span>
+			<span>-</span>
 			<p>crafted by <a href="http://github.com/bangnokia/pekyll" target="_blank">pekyll</a> with ❤️.</p>
 		</footer>
 	</div>
