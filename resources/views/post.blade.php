3b5260fa45bec20cb9cf01ac66478d047b72a005
bangnokia <bangnokia@gmail.com>
2021-04-07 11:46:51 +0700

    add comment to website

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,9 +4,9 @@
         <link rel="stylesheet" href="https://unpkg.com/@highlightjs/cdn-assets@10.6.0/styles/default.min.css">
     @endpush
 
-	<div id="post">
-	    <h1 class="text-cyan-600 text-3xl mb-5 font-bold">{{ $post->title }}</h1>
-	    <div class="text-gray-400 text-sm mb-10">
+    <div id="post">
+        <h1 class="text-cyan-600 text-3xl mb-5 font-bold">{{ $post->title }}</h1>
+        <div class="text-gray-400 text-sm mb-10">
             <span>Published on</span>
             <time datetime="{{ $post->created_at }}" class="tracking-tight">
                 {{ date('M, d Y', strtotime($post->created_at)) }}
@@ -22,17 +22,33 @@
             </div>
         </div>
 
-	    <!-- tags -->
-	    @if ($post->tags)
-	    	<div class="mt-5">
-	    		<strong>Tags:</strong>
-	    		@foreach ($post->tags as $tag)
-	    			<span class="tag bg-gray-200 px-2 py-1 cursor-pointer hover:bg-red-300">{{ $tag }}</span>
-	    		@endforeach
-	    	</div>
-	    @endif
+        <!-- tags -->
+        @if ($post->tags)
+            <div class="mt-10">
+                <strong>Tags:</strong>
+                @foreach ($post->tags as $tag)
+                    <span class="tag bg-gray-200 px-2 py-1 cursor-pointer hover:bg-red-300">{{ $tag }}</span>
+                @endforeach
+            </div>
+        @endif
 
-	</div>
+        <!-- comment box -->
+        <div id="disqus_thread" class="mt-20"></div>
+        <script>
+            var disqus_config = function () {
+                this.page.url = '{{ 'https://daudau.cc' . $post->link() }}';
+                this.page.identifier = '{{ $post->slug }}';
+            };
+            (function() {
+                var d = document, s = d.createElement('script');
+                s.src = 'https://daudau.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+    </div>
 
     @push('scripts')
         <script src="https://unpkg.com/@highlightjs/cdn-assets@10.6.0/highlight.min.js"></script>
