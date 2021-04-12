c50eb4e16e918078c3d7eb5e0f59673582717d82
bangnokia <bangnokia@gmail.com>
2021-04-12 21:18:34 +0700

    remove disqus

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -32,22 +32,6 @@
             </div>
         @endif
 
-        <!-- comment box -->
-        <div id="disqus_thread" class="mt-20"></div>
-        <script>
-            var disqus_config = function () {
-                this.page.url = '{{ 'https://daudau.cc' . $post->link() }}';
-                this.page.identifier = '{{ $post->slug }}';
-            };
-            (function() {
-                var d = document, s = d.createElement('script');
-                s.src = 'https://daudau.disqus.com/embed.js';
-                s.setAttribute('data-timestamp', +new Date());
-                (d.head || d.body).appendChild(s);
-            })();
-        </script>
-        <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-
     </div>
 
     @push('scripts')
