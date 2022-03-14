2910150a16aad4857568feeaf0cf996e25f1b674
bangnokia <bangnokia@gmail.com>
2022-03-15 04:06:41 +0700

    work in progress

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,21 @@
 <script>
-    import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="antialiased font-mono mx-auto flex flex-col min-h-screen px-5 pt-5 max-w-2xl">
+	<div class="flex space-x-5 flex-none">
+		<a href="/" class="text-slate-700 hover:underline">home</a>
+		<a href="/projects" class="text-slate-700 hover:underline">projects</a>
+		<a href="/about" class="text-slate-700 hover:underline">about me</a>
+	</div>
+	<div class="mt-20 flex-grow flex-shrink flex-1">
+		<slot />
+	</div>
+	<footer class="text-sm flex justify-end h-12 border-t pt-3 mt-10 space-x-2">
+		<p>
+			crafted by <a href="https://kit.svelte.dev">Sveltekit</a>
+			and <a href="https://tailwindcss.com">Tailwind CSS</a>
+			with ❤️.
+		</p>
+	</footer>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<ul>
+	<li class="mt-8">
+		<div class=" text-sm flex space-x-5 content-end">
+			<div class="text-gray-400">[<time datetime="2021-04-22">2021-04-22</time>]</div>
+			<div class="text-gray-600" />
+		</div>
+		<a
+			href="/posts/free-comment-service-for-technical-blog.html"
+			class="mt-5 text-sky-500 text-base"
+		>
+			The best &quot;Free&quot; comment service for technical blog
+		</a>
+	</li>
+</ul>
