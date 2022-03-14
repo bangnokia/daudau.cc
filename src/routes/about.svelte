0106be82e57bc87b6602b330a1ec12223d129b38
bangnokia <bangnokia@gmail.com>
2022-03-15 04:52:20 +0700

    fix grammer about page

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -12,30 +12,39 @@
 	<h1 class="text-xl font-semibold mt-10">
 		Hello, my name is <span class="text-cyan-500">Bang Nguyen Viet</span>
 	</h1>
+
 	<div class="mt-10 leading-7">
-		I'm a developer from Viet Nam.
-		<br />
-		I do stuff about <span class="text-red-500">Laravel</span>, sometimes
-		<span class="text-red-500">VueJS</span>
-		or <span class="text-red-500">ReactJS</span>.
-		<br />
-		On my freetimes, learning other languages is my hobbit. I know a little about
-		<a href="https://rust-lang.org" title="Rust programming language">Rust</a>
-		and even <span>Chinese</span> xD.
+		<p>I'm a developer from Vietnam. I'm a Full Stack Overflow developer with a passion for web development.</p>
+		<p class="mt-10">I love creating blazing fast and easy-to-use things.</p>
 	</div>
-	<div class="mt-10 leanding-7">
-		You can get in touch with me via <a href="mailto:bang@daudau.cc">bang@daudau.cc</a>,
-		<a href="https://github.com/bangnokia" target="_blank">Github</a>
-		or <a href="https://twitter.com/bangnokia" target="_blank">Twitter</a>.
-	</div>
-	<div class="mt-10 leading-7">
-		I'd like to play
-		<a href="https://store.steampowered.com/app/570/Dota_2/" target="_blank">dota2</a>
-		and
-		<a href="https://store.steampowered.com/app/550/Left_4_Dead_2/" target="_blank">Left 4 dead 2</a>
-		.
-		<br />
-		If you like that, we can play together on
-		<a href="https://steamcommunity.com/id/bangnokia/" target="_blank">Steam</a>.
+	<div class="mt-10 leading-7 flex flex-col gap-y-10 w-full">
+		<p>
+			I work with <span class="text-rose-500">Laravel</span> and occasionally <span class="text-rose-500">VueJS</span>
+			or
+			<span class="text-rose-500">ReactJS</span>, but now I've fallen in love with
+			<span class="text-rose-500 hover:animate-ping inline-block">Svelte</span>.
+		</p>
+
+		<p>
+			In my free time, learning other languages is my hobby. I know a little about
+			<a href="https://rust-lang.org" title="Rust programming language">Rust</a>
+			and even a little bit about <span>Chinese</span> xD.
+		</p>
+
+		<p>
+			You can get in touch with me via <a href="mailto:bang@daudau.cc">bang@daudau.cc</a>,
+			<a href="https://github.com/bangnokia" target="_blank">Github</a>, or
+			<a href="https://twitter.com/bangnokia" target="_blank">Twitter</a>.
+		</p>
+
+		<p>
+			I'd like to play
+			<a href="https://store.steampowered.com/app/570/Dota_2/" target="_blank">Dota 2</a>
+			and
+			<a href="https://store.steampowered.com/app/550/Left_4_Dead_2/" target="_blank">Left 4 Dead 2</a>.
+			<br />
+			If you like that, we can play together on
+			<a href="https://steamcommunity.com/id/bangnokia/" target="_blank">Steam</a>.
+		</p>
 	</div>
 </div>
