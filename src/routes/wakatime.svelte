b03c8c4d9ece321ec8991e726d5a079834ef19c7
bangnokia <bangnokia@gmail.com>
2022-07-14 23:30:26 +0700

    blog robots index my wakatime page

--- src/routes/wakatime.svelte
+++ src/routes/wakatime.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 </script>
 
+<svelte:head>
+	<meta name="robots" content="noindex" />
+</svelte:head>
+
 <div class="grid grid-cols-1 gap-4">
 	<figure><embed src="https://wakatime.com/share/@bangnokia/c4b1357a-ed5a-4974-9bbd-72cbe9544aa3.svg" /></figure>
 	<figure><embed src="https://wakatime.com/share/@bangnokia/0496729c-9b27-4123-a747-c4447300d13c.svg" /></figure>
