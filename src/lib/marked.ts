d6b204bc1d2d488fbb626c9b1d591ecf625ad711
bangnokia <bangnokia@gmail.com>
2023-06-13 14:22:24 +0700

    wip

--- src/lib/marked.ts
+++ src/lib/marked.ts
@@ -1,23 +1,21 @@
 import { Marked } from "marked"
-import { markedHighlight } from 'marked-highlight';
-import hljs from 'highlight.js';
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js'
 
 const marked = new Marked({
   langPrefix: '',
   mangle: false,
   headerIds: false
-});
+})
 
 marked.use(
   markedHighlight({
     langPrefix: 'hljs language-',
     highlight(code, lang) {
-      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-      return hljs.highlight(code, { language }).value;
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
     }
   })
-);
+)
 
-
-export default marked;
-
+export default marked
