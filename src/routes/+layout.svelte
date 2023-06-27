a50fad51c784ea5193d6196aacaf835576c3b6c5
bangnokia <bangnokia@gmail.com>
2023-06-27 23:47:16 +0700

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 	import 'virtual:uno.css';
 </script>
 
-<main class="w-full h-full bg-neutral-900">
+<main id="app" class="w-full h-full bg-neutral-900 text-neutral-400">
 	<div class="antialiased font-sans mx-auto flex flex-col min-h-screen px-5 pt-5 max-w-2xl overflow-y-hidden">
 		<nav class="flex space-x-5">
 			<a href="/" class="text-gray-300" data-sveltekit-preload-data="hover">blog</a>
@@ -13,8 +13,9 @@
 		<div class="mt-24 flex-grow flex-shrink flex-1">
 			<slot />
 		</div>
-		<footer class="text-xs flex justify-end h-12 border-t border-slate-200 pt-3 mt-10 space-x-2">
-			<a href="/wakatime" class="text-white">wakatime</a>
+		<footer class="text-xs flex justify-between h-12 border-t border-neutral-500 pt-3 mt-10 space-x-2">
+			<p>&copy; Nguyen Viet</p>
+			<a href="/wakatime" class="text-neutral-900">wakatime</a>
 		</footer>
 	</div>
 </main>
