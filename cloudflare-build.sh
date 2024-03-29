87fb2a261d440e5cd30fefd0d619340a2868da85
bangnokia <bangnokia@gmail.com>
2024-03-30 02:05:40 +0700

    wip

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -4,4 +4,4 @@
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
 curl -o lina https://raw.githubusercontent.com/bangnokia/lina/main/builds/lina
 
-./php lina
+./php lina build
