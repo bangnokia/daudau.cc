2e3b8ecc69dc8e96c05f2c4029d835cee3595c31
bangnokia <bangnokia@gmail.com>
2024-03-30 02:43:27 +0700

    wip

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,7 +15,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..700;1,100..700&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="/style.css">
+    <link rel="stylesheet" href="/style.css?t={{ time() }}">
 </head>
 
 <body>
