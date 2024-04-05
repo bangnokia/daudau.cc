9538d1a5b61bdca298f7a8615680e485612277cc
bangnokia <bangnokia@gmail.com>
2024-04-06 01:07:36 +0700

    wip

--- cloudflare-page.sh
+++ cloudflare-page.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-## download the PHP binary
+# download the PHP binary
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
 
-## download the lina phar
+# download the lina phar
 curl -o lina https://raw.githubusercontent.com/bangnokia/lina/releases/latest/download/lina.phar
 
+# Build static files
 ./php lina build
