17aaaf7a9045897ae6df84853bc0ff1e414e3783
bangnokia <bangnokia@gmail.com>
2024-04-06 01:17:05 +0700

    fix build command

--- cloudflare-page.sh
+++ cloudflare-page.sh
@@ -4,7 +4,7 @@
 curl -sL https://dl.static-php.dev/static-php-cli/common/php-8.3.4-cli-linux-x86_64.tar.gz | tar xz
 
 # download the lina phar
-curl -o lina https://raw.githubusercontent.com/bangnokia/lina/releases/latest/download/lina.phar
+curl -o lina https://github.com/bangnokia/lina/releases/latest/download/lina.phar
 
 # Build static files
 ./php lina build
