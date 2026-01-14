a496783b6bc1cc5966eb3db581a2371ac67cbc1e
bangnokia <bangnokia@gmail.com>
2026-01-14 23:41:29 +0700

    wip

--- resources/views/webding.blade.php
+++ resources/views/webding.blade.php
@@ -340,8 +340,8 @@
                 <svg viewBox="0 0 24 24" width="24" height="24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
             </button>
             <div class="qr-container">
-                <img src="/images/qr-bang.png" alt="QR Code Bang">
                 <img src="/images/qr-tinh.png" alt="QR Code Tinh">
+                <img src="/images/qr-bang.png" alt="QR Code Bang">
             </div>
         </div>
     </div>
